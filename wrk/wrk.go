75fa8ba86eb360400da6eeb1987c56ec29c4fb4e
Tossapon Nuanchuay <ntossapo@gmail.com>
2016-12-28 23:06:48 +0700

    forgot to edit ignore file and add new feature file

---
+++ wrk/wrk.go
@@ -0,0 +1,35 @@
+package wrk
+
+import (
+	"github.com/tspn/wrk-load-testing-module/model"
+	"os/exec"
+	"fmt"
+	"bufio"
+	"strings"
+)
+
+func Run(url, t, c, d string) model.WrkResult{
+	var command *exec.Cmd
+
+	command = exec.Command("wrk", "-t"+t, "-c"+c, "-d"+d ,url)
+
+	fmt.Println(command.Args)
+	cmdReader, _ := command.StdoutPipe()
+	scanner := bufio.NewScanner(cmdReader)
+	var out string
+	go func() {
+		for scanner.Scan() {
+			out = fmt.Sprintf("%s\n%s", out, scanner.Text())
+			if strings.Contains(out, "Transfer"){
+				break;
+			}
+		}
+	}()
+	command.Start()
+	command.Wait()
+	fmt.Println(out)
+
+	wrk := model.WrkResult{}
+	wrk.SetData(url, out)
+	return wrk
+}
