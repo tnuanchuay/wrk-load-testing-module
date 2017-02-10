13a843893595436f1fea87e1b4470dcece5eddd9
Tossapon Nuanchuay <ntossapo@gmail.com>
2017-02-10 14:47:41 +0700

    fix test after change unit k to K

--- unit/si/si_test.go
+++ unit/si/si_test.go
@@ -2,56 +2,56 @@ package si
 
 import "testing"
 
-func TestItShouldReturn1000WhenEnter1k(t *testing.T){
-	input  := "1k"
+func TestItShouldReturn1000WhenEnter1k(t *testing.T) {
+	input := "1K"
 	expected := 1000.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
 
-func TestItShouldReturn2000WhenEnter2k(t *testing.T){
-	input  := "2k"
+func TestItShouldReturn2000WhenEnter2k(t *testing.T) {
+	input := "2K"
 	expected := 2000.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
 
-func TestItShouldReturn2200WhenEnter2dot2k(t *testing.T){
-	input  := "2.2k"
+func TestItShouldReturn2200WhenEnter2dot2k(t *testing.T) {
+	input := "2.2K"
 	expected := 2200.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
 
-func TestItShouldReturn2200000WhenEnter2dot2M(t *testing.T){
-	input  := "2.2M"
+func TestItShouldReturn2200000WhenEnter2dot2M(t *testing.T) {
+	input := "2.2M"
 	expected := 2200000.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
 
-func TestItShouldReturn2WhenEnter2(t *testing.T){
-	input  := "2"
+func TestItShouldReturn2WhenEnter2(t *testing.T) {
+	input := "2"
 	expected := 2.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
 
-func TestItShouldReturn2200000WhenEnter2dot2MB(t *testing.T){
-	input  := "2.2MB"
+func TestItShouldReturn2200000WhenEnter2dot2MB(t *testing.T) {
+	input := "2.2MB"
 	expected := 2200000.0
 	result, _ := SIToFloat(input)
-	if expected != result{
+	if expected != result {
 		t.Error("expect", expected, "but return", result)
 	}
 }
