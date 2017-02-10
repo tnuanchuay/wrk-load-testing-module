e59eaf76168a012e6de121fc58703b4de5d70380
Tossapon Nuanchuay <ntossapo@gmail.com>
2017-02-10 14:18:07 +0700

    remove si bug

--- unit/si/si.go
+++ unit/si/si.go
@@ -1,33 +1,33 @@
 package si
 
 import (
-	"strings"
 	"strconv"
+	"strings"
 )
 
 const (
-	k	=	1000.0
-	M	=	1000000.0
-	G	=	1000000000.0
+	K = 1000.0
+	M = 1000000.0
+	G = 1000000000.0
 )
 
-var listUnits = []string{"k", "M", "G"}
+var listUnits = []string{"K", "M", "G"}
 var units = map[string]float64{
-	"k":k,
-	"M":M,
-	"G":G,
+	"K": K,
+	"M": M,
+	"G": G,
 }
 
-func SIToFloat(s string)(float64, error){
+func SIToFloat(s string) (float64, error) {
 	var result float64
 	ss := string(s)
-	for _, unit := range listUnits{
+	for _, unit := range listUnits {
 		if strings.Contains(ss, unit) {
 			v := strings.Split(ss, unit)[0]
 			vv, err := strconv.ParseFloat(v, 64)
-			if err != nil{
+			if err != nil {
 				return 0, err
-			}else{
+			} else {
 				result = float64(vv) * units[unit]
 				return result, nil
 			}
